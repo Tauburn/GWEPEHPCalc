66cf217a0106fcc3c4c311b9fbf3be41aef8c830
david.woodward <david.woodward@digital.hmrc.gov.uk>
2016-10-17 08:49:57 +0100

    Changes before re-imaging.

--- calculator-fe/app/connector/ApiConnector.scala
+++ calculator-fe/app/connector/ApiConnector.scala
@@ -1,24 +1,19 @@
-package connector
-
-import scalaj.http.{Http, HttpRequest, HttpResponse}
-
-/**
-  * Created by david on 09/06/16.
-  */
-object ApiConnector extends ApiConnector{
-
-}
-
-trait ApiConnector {
-
-  def getItem(): String = {
-    val response: HttpResponse[String] = Http("https://api.guildwars2.com/v2/items/5777").asString
-    print(response.body)
-    response.body
-  }
-
-  def getArmourStats(): String = {
-    val response: HttpResponse[String] = Http("https://api.guildwars2.com/v2/").asString
-    response.body
-  }
-}
+//package connector
+//
+//import scala.util.parsing.json.JSONObject
+//import scalaj.http.{Http, HttpRequest, HttpResponse}
+//
+///**
+//  * Created by david on 09/06/16.
+//  */
+//object ApiConnector extends ApiConnector{
+//
+//}
+//
+//trait ApiConnector {
+//
+//  def getItem(id: String): String = {
+//    val response: HttpResponse[HttpResponse] = Http("https://api.guildwars2.com/v2/items/" + id).asString
+//    JSONObject(response.body)
+//  }
+//}
